4e7393e23a26c371b4dcdfb012da93e14cb0e36f
Alex Tu <alex.lei.tu@gmail.com>
2017-01-11 17:13:01 +0900

    add new

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,6 +14,16 @@ yum_repository 'msprod' do
 end
 
 ENV['ACCEPT_EULA']='Y'
-package 'mssql-tools' do
-  action :install
+%w{ mssql-tools unixODBC-utf16-devel }.each do |pkg|
+    package pkg do
+          action :install
+            end
+end
+
+link '/usr/bin/sqlcmd' do
+    to '/opt/mssql-tools/bin/sqlcmd-13.0.1.0'
+end
+
+link '/usr/bin/bcp' do
+    to '/opt/mssql-tools/bin/sqlcmd-13.0.1.0'
 end
