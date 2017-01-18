bb60b88fb938a793a5443ba08d2ba2cd7f4634fa
Alex Tu <alex.lei.tu@gmail.com>
2017-01-18 11:19:18 +0900

    correct typo

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'alex.lei.tu@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures mssqltools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
+source_url       'https://github.com/leitu/mssqltools-cookbook'
+issues_url       'https://github.com/leitu/mssqltools-cookbook/issues'
 
 depends 'yum'
